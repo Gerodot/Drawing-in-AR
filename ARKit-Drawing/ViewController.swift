a9622453f78cda1fe82bdca2887edaca811196d0
Gerodot <gerodot@me.com>
2022-03-26 21:36:50 +0300

    Add node to the scene

--- ARKit-Drawing/ViewController.swift
+++ ARKit-Drawing/ViewController.swift
@@ -3,7 +3,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     // MARK: - Outlets
     @IBOutlet var sceneView: ARSCNView!
     
@@ -16,14 +16,76 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     var objectMode: ObjectPlacementMode = .freeform
     
+    var selectedNode: SCNNode?
+    
     // MARK: - Metods
+    
+    /// Add node in 20cm front of camera
+    /// - Parameter node: node of object to add
+    func addNodeInFront (_ node: SCNNode) {
+        // Get camera frame
+        guard let frame = sceneView.session.currentFrame else {return}
+        
+        // Get transtorm properiry from camera frame
+        let transform = frame.camera.transform
+        
+        // Create translation matrix
+        var translation = matrix_identity_float4x4
+        
+        // Translate by 20cm on z axis
+        translation.columns.3.z = -0.2
+
+        
+        // TASK: - Read documentation of  translation matrix
+        // Rotate by pi / 2 on z axis
+        translation.columns.0.x = 0
+        translation.columns.1.x = -1
+        translation.columns.0.y = 1
+        translation.columns.1.y = 0
+        
+        // Assign transform to node
+        node.simdTransform = matrix_multiply(transform, translation)
+        
+        // Add node to the scene
+        addNodeToSceneRoot(node)
+    }
+    
+    // Add node to ccene root
+    func addNodeToSceneRoot(_ node: SCNNode) {
+        // Clone node to separete copies of objects
+        let clonedNode = node.clone()
+        
+        // Add cloneNode to scene
+        sceneView.scene.rootNode.addChildNode(clonedNode)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showOptions" {
             let optionsViewController = segue.destination as! OptionsContainerViewController
             optionsViewController.delegate = self
         }
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        guard
+            let touch = touches.first,
+            let selectedNode = selectedNode
+        else {return}
+        
+        print(#line, #function, dump(touch))
+        
+        switch objectMode {
+        case .freeform:
+            addNodeInFront(selectedNode)
+        case .plane:
+            break
+        case .image:
+            break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -61,6 +123,7 @@ extension ViewController: OptionsViewControllerDelegate {
     
     func objectSelected(node: SCNNode) {
         dismiss(animated: true, completion: nil)
+        selectedNode = node
     }
     
     func togglePlaneVisualization() {
@@ -68,7 +131,6 @@ extension ViewController: OptionsViewControllerDelegate {
     }
     
     func undoLastObject() {
-        
     }
     
     func resetScene() {

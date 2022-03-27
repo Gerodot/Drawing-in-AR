a751289ab59a4f7c31637b7f8f51ae8af3c3811e
Gerodot <gerodot@me.com>
2022-03-27 22:08:48 +0300

    Hide planes Hide planes on change object mode Add holdTappOnScreen function for ads multiple objects Add minimum distance of objects Add people occlusion

--- ARKit-Drawing/ViewController.swift
+++ ARKit-Drawing/ViewController.swift
@@ -8,12 +8,27 @@ class ViewController: UIViewController {
     @IBOutlet var sceneView: ARSCNView!
     
     // MARK: - Properies
+    
+    // visualise planes
+    var arePlanesHidden = true {
+        didSet {
+            planeNodes.forEach { $0.isHidden = arePlanesHidden }
+        }
+    }
+    
     let configuration = ARWorldTrackingConfiguration()
+    
+    // LAst node placed by user
+    var lastNode: SCNNode?
+    
+    //Set minimum distanse betwen objects when moved
+    let minimumDistanse: Float = 0.05
     
     enum ObjectPlacementMode {
         case freeform, plane, image
     }
     
+    // Set default object mode
     var objectMode: ObjectPlacementMode = .freeform
     
     /// Arrays of  objects placed
@@ -43,8 +58,26 @@ class ViewController: UIViewController {
     }
     
     func addNode (_ node: SCNNode, to parentNode: SCNNode) {
+        // CHek the object is not to close to the previus one
+        if let lastNode = lastNode {
+            let lastPosition = lastNode.position
+            let newPosition = node.position
+            
+            let x = lastPosition.x - newPosition.x
+            let y = lastPosition.y - newPosition.y
+            let z = lastPosition.z - newPosition.z
+            
+            let distanceSquare = (x * x) + (y * y) + (z * z)
+            let minimumDistanceSquare =  minimumDistanse * minimumDistanse
+            
+            guard minimumDistanceSquare < distanceSquare else {return}
+        }
+        
         // Clone node to separete copies of objects
         let clonedNode = node.clone()
+        
+        // Remeber last placed node
+        lastNode = clonedNode
         
         // Remember object placed for undo
         objetsPlaced.append(clonedNode)
@@ -87,6 +120,15 @@ class ViewController: UIViewController {
         addNode(node, to: sceneView.scene.rootNode)
     }
     
+    // Add people occlusion
+    func peopleOcclusionAdd(){
+        if #available(iOS 13.0, *) {
+            configuration.frameSemantics.insert(.personSegmentationWithDepth)
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showOptions" {
             let optionsViewController = segue.destination as! OptionsContainerViewController
@@ -94,14 +136,7 @@ class ViewController: UIViewController {
         }
     }
     
-    func reloadConfiguretion () {
-        configuration.planeDetection = .horizontal
-        sceneView.session.run(configuration)
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        
+    func pressedScreen(_ touches: Set<UITouch>) {
         guard
             let touch = touches.first,
             let selectedNode = selectedNode
@@ -116,6 +151,24 @@ class ViewController: UIViewController {
         case .image:
             break
         }
+    }
+    
+    func reloadConfiguretion () {
+        configuration.planeDetection = .horizontal
+        peopleOcclusionAdd()
+        sceneView.session.run(configuration)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        lastNode = nil
+        pressedScreen(touches)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        
+        pressedScreen(touches)
     }
     
     override func viewDidLoad() {
@@ -140,10 +193,13 @@ class ViewController: UIViewController {
         switch sender.selectedSegmentIndex {
         case 0:
             objectMode = .freeform
+            arePlanesHidden = true
         case 1:
             objectMode = .plane
+            arePlanesHidden = false
         case 2:
             objectMode = .image
+            arePlanesHidden = true
         default:
             break
         }
@@ -160,6 +216,8 @@ extension ViewController: OptionsViewControllerDelegate {
     
     func togglePlaneVisualization() {
         dismiss(animated: true, completion: nil)
+        guard objectMode == .plane else {return}
+        arePlanesHidden.toggle()
     }
     
     func undoLastObject() {
@@ -193,6 +251,7 @@ extension ViewController: ARSCNViewDelegate {
     
     func nodeAdded(_ node: SCNNode, for anchor: ARPlaneAnchor){
         let planeNode = createFloor(planeAnchor: anchor)
+        planeNode.isHidden = arePlanesHidden
         
         // Add plane node to the list of plane nodes
         planeNodes.append(planeNode)
